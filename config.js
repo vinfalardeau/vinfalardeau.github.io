63c108f68025e0880c45ea37cdb1eaa221e344bf
vinfalardeau <79486268+vinfalardeau@users.noreply.github.com>
2023-01-05 14:01:21 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,112 @@
+var config = {
+    style: 'mapbox://styles/mapbox/satellite-v9',
+    accessToken: 'pk.eyJ1IjoidmluY2VudHJodW1ibGluZSIsImEiOiJjbGNpNHZoam4wOGk5M29wZXFsaGdvdWViIn0.XvKO9DMeISCMCch_tNqUsg',
+    showMarkers: false,
+    markerColor: '#3FB1CE',
+    //projection: 'equirectangular',
+    //Read more about available projections here
+    //https://docs.mapbox.com/mapbox-gl-js/example/projections/
+    inset: true,
+    theme: 'dark',
+    use3dTerrain: false, //set true for enabling 3D maps.
+    auto: false,
+    title: 'Birds of the Texas Islands',
+    subtitle: 'Audubon Texas protects bird habitat on nearly 200 islands in the Gulf of Mexico',
+    byline: 'By Rhumbline Maps',
+    footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
+    chapters: [
+        {
+            id: 'slug-style-id',
+            alignment: 'left',
+            hidden: false,
+            title: 'The Texas Coast',
+            image: './path/to/image/source.png',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            location: {
+                center: [-96.41804, 28.40959],
+                zoom: 7.50,
+                pitch: 51.36,
+                bearing: -43.20
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 1,
+                //     duration: 5000
+                // }
+            ],
+            onChapterExit: [
+                // {
+                //     layer: 'layer-name',
+                //     opacity: 0
+                // }
+            ]
+        },
+        {
+            id: 'second-identifier',
+            alignment: 'right',
+            hidden: false,
+            title: 'Second Title',
+            image: './path/to/image/source.png',
+            description: 'Copy these sections to add to your story.',
+            location: {
+                center: [-97.16936, 26.22568],
+                zoom: 10.80,
+                pitch: 58.00,
+                bearing: -61.60,
+                // flyTo additional controls-
+                // These options control the flight curve, making it move
+                // slowly and zoom out almost completely before starting
+                // to pan.
+                speed: 2, // make the flying slow
+                //curve: 1, // change the speed at which it zooms out
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: true,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'third-identifier',
+            alignment: 'left',
+            hidden: false,
+            title: 'Third Title',
+            image: './path/to/image/source.png',
+            description: 'Copy these sections to add to your story.',
+            location: {
+                center: [-97.37563, 26.79609],
+                zoom: 10.33,
+                pitch: 57.56,
+                bearing: -28.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'fourth-chapter',
+            alignment: 'fully',
+            hidden: false,
+            title: 'Third Title',
+            image: './path/to/image/source.png',
+            description: 'Copy these sections to add to your story.',
+            location: {
+                center: [-97.27576, 27.42893],
+                zoom: 11.54,
+                pitch: 55.42,
+                bearing: -7.20
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        }
+    ]
+};
